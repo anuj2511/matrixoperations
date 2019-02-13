cd9271051c7709b3b176d1854651547bc107f332
mayurd31 <mayurdd31@gmail.com>
2019-02-13 23:30:20 +0530

    update gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -5,23 +5,23 @@ int main(){
 
     std::cout<<"Entered Main.\n";
 
-    mayur::Matrix A(2,2);
+    mayur::Matrix A(3,2);
     A.getMatrix();
 
-    mayur::Matrix B(2,2);
+    mayur::Matrix B(2,3);
     B.getMatrix();
 
     mayur::Matrix C, D, E;
     
-    C = A + B ;
-    D = A - B ;
+    //C = A + B ;
+    //D = A - B ;
     E = A * B ;
 
-    std::cout<<"addition: \n";
-    C.displayMatrix();
+    //std::cout<<"addition: \n";
+    //C.displayMatrix();
 
-    std::cout<<"\nsubtraction \n";
-    D.displayMatrix();
+    //std::cout<<"\nsubtraction \n";
+    //D.displayMatrix();
 
     std::cout<<"\nmultiplication : \n";
     E.displayMatrix();
