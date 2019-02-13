05f0e6becd8dee02eb6c9f03537793d77d5e77dc
mayurd31 <mayurdd31@gmail.com>
2019-02-13 20:11:37 +0530

    added code

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -2,16 +2,22 @@
 #include"matrix.h"
 
 namespace mayur{
+
+    Matrix::Matrix(int i, int j){
+	    row = i;
+	    column = j;
+    }
+
+    //accept matrix
     void Matrix::getMatrix(){
         int i,j;
 
         for(i=0;i<column;i+=1){
             for(j=0;j<row; j+=1){
-                std::cout<<"\nEnter the element no "<<i+1<<" and "<<j+1<<" .   ";
+                std::cout<<"\nEnter the element no "<<i+1<<" "<<j+1<<".  ";
                 std::cin>>matrix[i][j];
             }
         }
-
     }
 
     // display method
@@ -25,7 +31,34 @@ namespace mayur{
             }
         std::cout<<"\n";
         }
+    }
 
+
+    Matrix Matrix::operator-(Matrix Z){
+
+        matrix Y(3,3);
+        int i, j;
+
+        for(i=0;i<column;i+=1){
+            for(j=0;j<row; j+=1){
+                Y.matrix1[i][j] = matrix[i][j] - Z.matrix[i][j];
+            }
+        }
+
+    }
+
+    Matrix Matrix::operator*(Matrix Z){
+
+        Matrix Y(3,3);
+        int i, j, k;
+
+        for(i=0;i<column;i+=1){
+            for(j=0;j<row; j+=1){
+                for(k=0; k<3; k+=1){
+                Y.matrix[i][j] += matrix[i][k] * Z.matrix[k][j];
+                }
+            }
+        }
     }
 
 }
